7fcc0063c9ff5f0cd5985cda6d4e8c5b3c1bebea
panchalharsh1991 <panchalharsh1991@gmail.com>
2016-08-01 11:32:11 -0500

    Checking change made in comments UserDetail

--- src/main/java/com/yash/rtd/model/UserDetail.java
+++ src/main/java/com/yash/rtd/model/UserDetail.java
@@ -6,7 +6,7 @@ import java.util.Date;
 /**
  * User Model will be used to hold the data from form. 
  * and perform form validation
- * @author manideep
+ * @author Harsh
  *
  */
 public class UserDetail {
